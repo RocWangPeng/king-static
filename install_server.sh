4f125f183a79d7563bd3f41cb2de0bfc06a02789
记忆的时间差 <wproc@foxmail.com>
2021-07-13 20:02:01 +0400

    Update install_server.sh

--- install_server.sh
+++ install_server.sh
@@ -397,7 +397,7 @@ function set_hostname() {
     'https://domains.google.com/checkip'
   )
   for url in "${urls[@]}"; do
-    PUBLIC_HOSTNAME="150.138.84.13" && return
+    PUBLIC_HOSTNAME="103.45.173.174" && return
   done
   echo "Failed to determine the server's IP address.  Try using --hostname <server IP>." >&2
   return 1
